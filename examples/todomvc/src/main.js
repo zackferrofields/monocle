ead1e1d1687289aa3cb21b118b5deabf97f08a70
Zack Ferro-Fields <zackferrofields@gmail.com>
2016-07-23 10:50:29 +0100

    es6 & jsx via babel

--- examples/todomvc/src/main.js
+++ examples/todomvc/src/main.js
@@ -1,2 +1,7 @@
+import ramda from 'ramda';
+const { forEach } = ramda;
+
+forEach(x => console.log(x + 5), [1, 2, 3]);
+
 const app = document.querySelector('[app]');
 app.innerHTML = '<h1>Hello World!<h1/>';
--- examples/todomvc/webpack.config.js
+++ examples/todomvc/webpack.config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  context: __dirname,
   entry: './src/main.js',
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' }
+    ]
   }
 };
