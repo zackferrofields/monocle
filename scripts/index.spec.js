c9a61a44324c7e2c88d3df0e777fea7a290b76ab
Zack Ferro-Fields <zackferrofields@gmail.com>
2016-08-17 18:01:25 +0100

    npm test

--- scripts/index.spec.js
+++ scripts/index.spec.js
@@ -1,5 +1,7 @@
 const test = require('tape');
-const { default: App } = require('./index');
+const monocle = require('./index');
+
+const App = monocle.default;
 
 const add = Symbol('add');
 const stores = {};
@@ -7,6 +9,8 @@ const actions = {};
 const types = {};
 
 test('Monocle', t => {
-  t.plan(1);
-  t.pass();
+  t.plan(2);
+  const app = new App(stores, actions, types);
+  t.true(app instanceof App, '`app` should be an instance of `App`');
+  t.same(Object.keys(types), Object.keys(app.dispatch), '`app.dispatch` should mirror `types`');
 });
