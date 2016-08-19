abc42f6f5b5a922cc11fcbf4f469dd9730287984
Zack Ferro-Fields <zackferrofields@gmail.com>
2016-08-19 15:57:48 +0100

    Update .lambci.js
    build `develop`

--- .lambci.js
+++ .lambci.js
@@ -1,5 +1,9 @@
 module.exports = {
   cmd: 'npm install && npm run test',
+  branches: {
+    master: true,
+    develop: true
+  },
   notifications: {
     slack: {
       channel: '#bots-monocle'
