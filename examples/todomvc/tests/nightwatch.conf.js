ed60808a79255cb236d87728ac7679935ebde617
Zack Ferro-Fields <zackferrofields@gmail.com>
2016-08-25 18:11:19 +0100

    remove log

--- examples/todomvc/tests/nightwatch.conf.js
+++ examples/todomvc/tests/nightwatch.conf.js
@@ -1,6 +1,5 @@
 const launch_url = process.env.APP_PORT_80_TCP_ADDR;
 const selenium_host = process.env.SELENIUMHUB_PORT_4444_TCP_ADDR;
-console.log(launch_url, selenium_host);
 
 module.exports = {
   'src_folders':[ require('nightwatch-cucumber')() ],
